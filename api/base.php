50d01a0ad7098a0a339c7c6548ff9f87a963f7a0
Xenophmix <xenophmix@gmail.com>
2022-12-26 09:49:00 +0800

    試做find函式，並建立資料表來驗證

--- api/base.php
+++ api/base.php
@@ -3,7 +3,8 @@
 session_start();
 date_default_timezone_set("Asia/Taipei");
 
-class DB {
+class DB
+{
   protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db10";
   protected $table;
   protected $pdo;
@@ -11,58 +12,69 @@ class DB {
   public function __construct($table)
   {
     $this->table = $table;
-    $this->pdo = new PDO($this->dsn,'root','');
+    $this->pdo = new PDO($this->dsn, 'root', '');
   }
 
 
-  public function find(){
+  public function find($id)
+  {
+    $sql = "SELECT * FROM $this->table";
+    if (is_array($id)) {
+      foreach ($id as $key => $value) {
+        $tmp[] = "`$key` = '$value'";
+      }
 
+      $sql = $sql . " WHERE " . join("&&", $tmp);
+    } else {
+      $sql = $sql . " WHERE `id` = '$id'";
+    }
+    return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
   }
 
-  public function all(){
-
+  public function all()
+  {
   }
 
-  public function save(){
-
+  public function save()
+  {
   }
 
-  public function del(){
-
+  public function del()
+  {
   }
 
-  public function sum(){
-
+  public function sum()
+  {
   }
 
-  public function count(){
-
+  public function count()
+  {
   }
 
-  public function max(){
-
+  public function max()
+  {
   }
 
-  public function min(){
-
+  public function min()
+  {
   }
 
-  public function avg(){
-
+  public function avg()
+  {
   }
 }
 
-function dd(){
-
-
+function dd()
+{
 }
 
-function to($location){
-
-
+function to($location)
+{
 }
 
-function q(){
-
-
+function q()
+{
 }
+$db = new DB('bottom');
+$bot = $db->find(1);
+print_r($bot);
