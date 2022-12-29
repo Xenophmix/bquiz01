2841dc4f2138eeb36550d0578abd929a1205cd13
Xenophmix <xenophmix@gmail.com>
2022-12-29 11:07:15 +0800

    完成標題管理的更改文字,顯示,及刪除功能,完成更新圖片功能

--- api/edit_title.php
+++ api/edit_title.php
@@ -1,27 +1,17 @@
 <?php
 include_once "base.php";
 
-dd($_POST);
-
-foreach($_POST['id'] as $idx => $id){
-    $row=$Title->find($id);
-    $row['text']=$_POST['text'][$idx];
-    $Title->save($row);
-}
-
-$row1=$Title->find($_POST['sh']);
-foreach($_POST['id'] as $id){
-    $row2=$Title->find($id);
-    $row2['sh']=0;
-    $Title->save($row2);
-}
-
-$row1['sh']=1;
-$Title->save($row1);
-
-foreach($_POST['del'] as $id){
-    $Title->del($id);
+// dd($_POST);
+foreach ($_POST['id'] as $idx => $id) {
+    if (isset($_POST['del']) && in_array($id, $_POST['del'])) {
+        $Title->del($id);
+    } else {
+        $row = $Title->find($id);
+        $row['text'] = $_POST['text'][$idx];
+        $row['sh'] = (isset($_POST['sh']) && $_POST['sh'] == $id) ? 1 : 0;
+        echo $row['sh'];
+        $Title->save($row);
+    }
 }
 
 to("../back.php?do=title");
-?>
--- back/title.php
+++ back/title.php
@@ -32,7 +32,7 @@
 							<input type="checkbox" name="del[]" value="<?= $row['id']; ?>">
 						</td>
 						<td>
-							<input type="button" value="更新圖片">
+							<input type="button" onclick="op('#cover','#cvr','./modal/upload_title.php?id=<?= $row['id']; ?>')" value="更新圖片">
 							<input type="hidden" name="id[]" value="<?= $row['id']; ?>">
 						</td>
 					</tr>
@@ -44,7 +44,7 @@
 		<table style="margin-top:40px; width:70%;">
 			<tbody>
 				<tr>
-					<td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/title.php')" value="新增網站標題圖片"></td>
+					<td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/add_title.php')" value="新增網站標題圖片"></td>
 					<td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
 				</tr>
 			</tbody>
