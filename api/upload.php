4cf242003de150682af7ab1dbbc13a87e60346c1
Xenophmix <xenophmix@gmail.com>
2022-12-29 15:10:36 +0800

    整合更新圖片的api為一支程式

--- api/upload.php
+++ api/upload.php
@@ -2,15 +2,15 @@
 include_once "./base.php";
 // echo $_POST['id'];
 
-
-$row = $Title->find($_POST['id']);
+$table = $_POST['table'];
+$row = $$table->find($_POST['id']);
 if (!empty($_FILES['img']['tmp_name'])) {
   move_uploaded_file($_FILES['img']['tmp_name'], '../upload/' . $_FILES['img']['name']);
   $row['img'] = $_FILES['img']['name'];
   dd($row);
-  $Title->save($row);
+  $$table->save($row);
 }
 
-to("../back.php?do=title");
+to("../back.php?do=" . lcfirst($table));
 
 
--- modal/upload_title.php
+++ modal/upload_title.php
@@ -1,6 +1,6 @@
 <h1>更新網站標題圖片</h1>
 <hr>
-<form action="./api/upload_title.php" method="POST" enctype="multipart/form-data">
+<form action="./api/upload.php" method="POST" enctype="multipart/form-data">
   <table>
     <tr>
       <td>標題區圖片:</td>
@@ -11,6 +11,7 @@
   </table>
   <div>
     <input type="hidden" name="id" value="<?=$_GET['id']?>">
+    <input type="hidden" name="table" value="Title">
     <input type="submit" value="更新">
     <input type="reset" value="重置">
   </div>
